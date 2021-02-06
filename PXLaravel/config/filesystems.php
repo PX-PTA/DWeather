ccda8a2c1bae4a9034ea8725ababd91444a3c32b
Aldi Afandi <mraldiafandi@gmail.com>
2021-02-06 19:50:31 +0700

    update

--- PXLaravel/config/filesystems.php
+++ PXLaravel/config/filesystems.php
@@ -37,9 +37,14 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path().'../../storage',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+        ],
+        
+        'public_uploads' => [
+            'driver' => 'local',
+            'root'   => public_path() . '../../storage',
         ],
 
         's3' => [
