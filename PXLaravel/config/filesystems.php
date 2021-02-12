35d278d68c5cef44943dfacbcdaad6d8ad54507e
primexaviers <mraldiafandi@gmail.com>
2021-02-13 01:34:15 +0700

    update

--- PXLaravel/config/filesystems.php
+++ PXLaravel/config/filesystems.php
@@ -41,11 +41,6 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
-        
-        'public_uploads' => [
-            'driver' => 'local',
-            'root'   => public_path() . '../../storage',
-        ],
 
         's3' => [
             'driver' => 's3',
